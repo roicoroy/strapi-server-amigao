141cf2a7329a67dd3b091b838cc2e3ed19eb97cb
Ricardo <roicoroy@yahoo.com.br>
2023-03-30 18:38:48 +0100

    welll known

--- src/plugins/strapi-stripe/server/middlewares/staticFiles.js
+++ src/plugins/strapi-stripe/server/middlewares/staticFiles.js
@@ -27,7 +27,7 @@ module.exports = async ({ strapi }) => {
       async handler(ctx, next) {
         ctx.url = path.basename(`${ctx.url}/assetlinks.json`);
         const folderPath = strapi.dirs.extensions || strapi.dirs.dist.extensions;
-        const staticFolder = path.resolve(folderPath, 'strapi-test', 'public');
+        const staticFolder = path.resolve(folderPath, 'strapi-well-known', 'public');
         return koaStatic(staticFolder)(ctx, next);
       },
       config: {
